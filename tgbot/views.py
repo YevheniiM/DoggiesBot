8eb39518ccb1852f81f10a5d5620c9b99bccd3d3
ymolodtsov <ymolodtsov.appwell@ancestry.com>
2022-01-30 16:20:11 +0100

    Change Procfile

--- tgbot/views.py
+++ tgbot/views.py
@@ -17,15 +17,14 @@ class TelegramBotWebhookView(View):
     # WARNING: if fail - Telegram webhook will be delivered again. 
     # Can be fixed with async celery task execution
     def post(self, request, *args, **kwargs):
-        process_telegram_event(json.loads(request.body))
-        # if DEBUG:
-        #     process_telegram_event(json.loads(request.body))
-        # else:
-        #     # Process Telegram event in Celery worker (async)
-        #     # Don't forget to run it and & Redis (message broker for Celery)!
-        #     # Read Procfile for details
-        #     # You can run all of these services via docker-compose.yml
-        #     process_telegram_event.delay(json.loads(request.body))
+        if DEBUG:
+            process_telegram_event(json.loads(request.body))
+        else:
+            # Process Telegram event in Celery worker (async)
+            # Don't forget to run it and & Redis (message broker for Celery)!
+            # Read Procfile for details
+            # You can run all of these services via docker-compose.yml
+            process_telegram_event.delay(json.loads(request.body))
 
         # TODO: there is a great trick to send action in webhook response
         # e.g. remove buttons, typing event
