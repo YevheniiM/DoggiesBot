501f113c5d074916a8006ca6b4a8e1e9ec94add6
ymolodtsov <ymolodtsov.appwell@ancestry.com>
2022-02-05 03:07:11 +0100

    Remove models path

--- dtb/settings.py
+++ dtb/settings.py
@@ -173,7 +173,7 @@ if TELEGRAM_TOKEN is None:
 
 TELEGRAM_LOGS_CHAT_ID = os.getenv("TELEGRAM_LOGS_CHAT_ID", default=None)
 
-MODELS_PATH = os.getenv("MODELS_PATH", default='/code/ai/ai_models/')
+# MODELS_PATH = os.getenv("MODELS_PATH", default='/code/ai/ai_models/')
 IMAGES_PATH = os.getenv("IMAGES_PATH", default='/media/images/')
 
 # -----> SENTRY
